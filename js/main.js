e52fca6c4555908e19cd6a03481254c678e86ffc
mo3az <51883008+mo3az96@users.noreply.github.com>
2020-04-17 19:01:29 +0200

    index

---
+++ js/main.js
@@ -0,0 +1,81 @@
+$(window).on("load", function () {
+
+});
+$(document).ready(function () {
+    new WOW().init();
+
+    //////////////////////////////////////////////
+    var mainswiper = new Swiper('.main-slider .swiper-container', {
+        pagination: {
+            el: '.main-slider .swiper-pagination',
+            clickable: true,
+        },
+        navigation: false,
+        autoplay: false,
+        spaceBetween: 10,
+        loop: true,
+        navigation: {
+            nextEl: '.main-slider .swiper-button-next',
+            prevEl: '.main-slider .swiper-button-prev',
+        },
+    });
+    /////////about Slider/////////
+    var aboutswiper = new Swiper('.about-slider .swiper-container', {
+        pagination: {
+            el: '.about-slider .swiper-pagination',
+            clickable: true,
+        },
+        navigation: false,
+        autoplay: false,
+        spaceBetween: 10,
+        loop: false,
+    });
+    if ($(window).width() <= 767) {
+        $('.second-header').wrap('<div class="xs-nav"></div>');
+
+
+        $('.mo-menu-icon').click(function () {
+            $(".xs-nav").fadeIn(400);
+            $(".second-header").addClass("nav-in");
+            $("body").addClass("overflow");
+        });
+
+        $('.xs-nav').click(function () {
+            $(".xs-nav").fadeOut(400);
+            $(".second-header").removeClass("nav-in");
+            $("body").removeClass("overflow");
+        });
+        $('.second-header').click(function (e) {
+            e.stopPropagation();
+        });
+        $('.close-btn').click(function () {
+            $(".xs-nav").fadeOut(400);
+            $(".second-header").removeClass("nav-in");
+            $(".lang").removeClass("lang-in");
+        });
+    }
+
+
+    $('.text-show').click(function () {
+        $(this).siblings(".span-hide").show();
+        $(this).hide();
+    });
+    var a = 0;
+    $(window).scroll(function () {
+        if (a == 0 && $(this).scrollTop() >= ($(".num").offset().top) - 500) {
+            $('.num').each(function () {
+                $(this).prop('Counter', 0).animate({
+                    Counter: $(this).text()
+                }, {
+                    duration: 1000,
+                    easing: 'swing',
+                    step: function (now) {
+                        $(this).text(Math.ceil(now));
+                    }
+                });
+            });
+            console.log($('.num'));
+            a++
+        }
+    });
+});
